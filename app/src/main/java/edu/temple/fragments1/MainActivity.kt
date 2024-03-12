5240c0e9dc260662bf185e70247dfb341e8461c3
Eric Reizas <tuo17224@temple.edu>
2024-03-12 13:44:47 -0400

    Added ImageFragment at runtime to the bottom half of the screen

--- app/src/main/java/edu/temple/fragments1/MainActivity.kt
+++ app/src/main/java/edu/temple/fragments1/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val colorFragment = ColorFragment()
+        supportFragmentManager.beginTransaction().add(R.id.fragmentContainerView2, colorFragment).commit()
     }
 }
