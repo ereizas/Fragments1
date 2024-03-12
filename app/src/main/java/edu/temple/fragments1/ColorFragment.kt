b8433aca9164d1e014d514ef3b9cb905ebe60ad6
Eric Reizas <tuo17224@temple.edu>
2024-03-12 12:49:45 -0400

    Set onClickListener for ColorFragment's layout

--- app/src/main/java/edu/temple/fragments1/ColorFragment.kt
+++ app/src/main/java/edu/temple/fragments1/ColorFragment.kt
@@ -1,7 +1,24 @@
 package edu.temple.fragments1
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
+
 class ColorFragment : Fragment() {
-    
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_color,container,false).apply{
+            this.setOnClickListener{
+                //Credit to https://stackoverflow.com/questions/5280367/android-generate-random-color-on-click for random color generation
+                val newColor = (Math.random() * 16777215).toInt() or (0xFF shl 24)
+                it.setBackgroundColor(newColor)
+            }
+        }
+    }
 }
